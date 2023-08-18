75c9cc8979fbd48c5920a552263bca93531d8f44
Kevin Chen <kcheart@gmail.com>
2023-08-18 10:04:35 +0800

    add while loop example, Euclidean algorithm

---
+++ while_loop2.c
@@ -0,0 +1,28 @@
+// 2023.08.18
+// while loop example: 輾轉相除法-求兩正整數最大公因數
+// Euclidean algorithm
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b, r, gcd;
+
+    printf("輸入第1個正整數: ");
+    scanf("%d", &a);
+    printf("輸入第2個正整數: ");
+    scanf("%d", &b);
+    printf("(%d, %d)= ", a, b);
+
+    r = a % b;
+    while (r != 0)
+    {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    gcd = b;
+    printf("%d\n", gcd);
+
+    return 0;
+}
