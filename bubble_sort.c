047996db8a1d9054fa175ca7567dc4840d2ef48c
Kevin Chen <kcheart@gmail.com>
2023-08-22 08:59:27 +0800

    add bubble sort example

---
+++ bubble_sort.c
@@ -0,0 +1,47 @@
+// 2003.08.20
+//  bubble sort
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define DATA_SIZE 20
+
+int main()
+{
+    int data[DATA_SIZE];
+
+    // srand(time(NULL));
+    int i;
+
+    printf("Before sorting:\n");
+    for (i = 0; i < DATA_SIZE; i++)
+    {
+        data[i] = rand() % 100;
+        printf("%-5d", data[i]);
+        if ((i + 1) % 10 == 0)
+            printf("\n");
+    }
+    printf("\n\n");
+
+    printf("After sorting:\n");
+    int j, temp;
+    for (i = 1; i < DATA_SIZE; i++)
+    {
+        for (j = 0; j < DATA_SIZE-i; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < DATA_SIZE; i++)
+    {
+        printf("%-5d", data[i]);
+        if ((i + 1) % 10 == 0)
+            printf("\n");
+    }
+    printf("\n");
+}
